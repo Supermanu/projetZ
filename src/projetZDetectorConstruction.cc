e847cda996945866b7ddb69e23e6646ca6ee848b
Manuel Tondeur <manueltondeur@gmail.com>
2012-04-05 15:50:56 +0200

    Division du calorimètre en 20 parties.

--- src/projetZDetectorConstruction.cc
+++ src/projetZDetectorConstruction.cc
@@ -38,7 +38,7 @@
 #include "G4ThreeVector.hh"
 #include "G4PVPlacement.hh"
 #include "globals.hh"
-#include <string> 
+#include <string>
 
 projetZDetectorConstruction::projetZDetectorConstruction()
 :  WorldVolume_log(0), tracker_log(0),
@@ -100,7 +100,7 @@ G4VPhysicalVolume* projetZDetectorConstruction::Construct()
   G4int Oradii[nbTracker]={8.1*cm,9.1*cm,10.1*cm,20.1*cm,30.1*cm,40.1*cm,50.1*cm,60.1*cm,70.1*cm,80.1*cm,90.1*cm,100.1*cm,110.1*cm};
   int const nbSteps=3;
   
-  std::string names[nbSteps][nbTracker] =
+  std::string namesTracker[nbSteps][nbTracker] =
   {"t1s","t2s","t3s","t4s","t5s","t6s","t7s","t8s","t9s","t10s","t11s","t12s","t13s","t1l","t2l","t3l","t4l","t5l","t6l","t7l","t8l","t9l","t10l","t11l","t12l","t13l","t1","t2","t3","t4","t5","t6","t7","t8","t9","t10","t11","t12","t13"} ; // J'ai inversé nbSteps et nbTracker pour bien définir l'array. J'ai répercuté le changement dans la boucle en bas.
   
   
@@ -112,46 +112,33 @@ G4VPhysicalVolume* projetZDetectorConstruction::Construct()
     G4double startAngleOfTheTube = 0.*deg;
     G4double spanningAngleOfTheTube = 360.*deg;
     
-    G4Tubs* tracker_Solid = new G4Tubs(names[0][i],innerRadiusOfTheTube,outerRadiusOfTheTube,hightOfTheTube,startAngleOfTheTube,spanningAngleOfTheTube);
+    G4Tubs* tracker_Solid = new G4Tubs(namesTracker[0][i],innerRadiusOfTheTube,outerRadiusOfTheTube,hightOfTheTube,startAngleOfTheTube,spanningAngleOfTheTube);
     
-    tracker_log = new G4LogicalVolume(tracker_Solid, Si,names[1][i],0,0,0); // fill the solid with "Silicon"       variable non declarée...
+    tracker_log = new G4LogicalVolume(tracker_Solid, Si,namesTracker[1][i],0,0,0); // fill the solid with "Silicon"       variable non declarée...
     // G4SensitiveDetector* trackerS_log = new   //  		!!!!! TO DO !!!
-    tracker = new G4PVPlacement(0,G4ThreeVector(),tracker_log, names[2][i],WorldVolume_log,false,0);   // raises it to physical volume
+    tracker = new G4PVPlacement(0,G4ThreeVector(),tracker_log, namesTracker[2][i],WorldVolume_log,false,0);   // raises it to physical volume
   }
   
   
   //------------------------------ a calorimeter tube
   
+  G4int nbrCalo = 20 ;
+  std::string nomsCalo[3][20] = { "cal01t", "cal02t", "cal03t", "cal04t", "cal05t", "cal06t", "cal07t", "cal08t" "cal09t", "cal10t", "cal11t", "cal12t", "cal13t", "cal14t", "cal15t", "cal16t", "cal17t", "cal18t", "cal19t", "cal20t","cal01l", "cal02l", "cal03l", "cal04l", "cal05l", "cal06l", "cal07l", "cal08l" "cal09l", "cal10l", "cal11l", "cal12l", "cal13l", "cal14l", "cal15l", "cal16l", "cal17l", "cal18l", "cal19l", "cal20l","cal01", "cal02", "cal03", "cal04", "cal05", "cal06", "cal07", "cal08" "cal09", "cal10", "cal11", "cal12", "cal13", "cal14", "cal15", "cal16", "cal17", "cal18", "cal19", "cal20" };
   G4double innerRadiusOfTheCalo = 1.5*m;
   G4double outerRadiusOfTheCalo = 3.*m;
   G4double hightOfTheCalo = 5.8*m;     //half length of the tube
-  G4double startAngleOfTheCalo = 0.*deg;
-  G4double spanningAngleOfTheCalo = 360.*deg;
   
-  G4Tubs* calorimeterBlock_tube = new G4Tubs("caloTubs",innerRadiusOfTheCalo,outerRadiusOfTheCalo,hightOfTheCalo,startAngleOfTheCalo,spanningAngleOfTheCalo);
+  for (int n = 0; n<20 ; n++)
+  {
+  G4double startAngleOfTheCalo = n*18.*deg;
+  G4double spanningAngleOfTheCalo = 18.*deg;
+  
+  G4Tubs* calorimeterBlock_tube = new G4Tubs(nomsCalo[0][n],innerRadiusOfTheCalo,outerRadiusOfTheCalo,hightOfTheCalo,startAngleOfTheCalo,spanningAngleOfTheCalo);
   calorimeterBlock_log = new G4LogicalVolume(calorimeterBlock_tube,
-					     Pb,"caloBlock_log",0,0,0);
-  calorimeterBlock_phys = new G4PVPlacement(0,G4ThreeVector(),calorimeterBlock_log,"caloBlock",WorldVolume_log,false,0);
-  
-  //------------------------------ calorimeter layers
-  
-//   G4double calo_x = 1.*cm;
-//   G4double calo_y = 40.*cm;
-//   G4double calo_z = 40.*cm;
-//   G4Box* calorimeterLayer_box = new G4Box("caloLayer_box",
-// 					  calo_x,calo_y,calo_z);
-//   calorimeterLayer_log = new G4LogicalVolume(calorimeterLayer_box,
-// 					     Al,"caloLayer_log",0,0,0);
-//   for(G4int i=0; i<19; i++) // loop for 19 layers
-//     {
-//       G4double caloPos_x = (i-9)*10.*cm;
-//       G4double caloPos_y = 0.0*m;
-//       G4double caloPos_z = 0.0*m;
-//       calorimeterLayer_phys = new G4PVPlacement(0,
-// 						G4ThreeVector(caloPos_x,caloPos_y,caloPos_z),
-// 						calorimeterLayer_log,"caloLayer",WorldVolume_log,false,i);
-//     }
-    
+					     Pb,nomsCalo[1][n],0,0,0);
+  calorimeterBlock_phys = new G4PVPlacement(0,G4ThreeVector(),calorimeterBlock_log,nomsCalo[2][n],WorldVolume_log,false,0);
+  }
+
     //------------------------------------------------------------------
     
     return World_Volume;
