9ddfe3dbf213f3163388f2db4c1942911b3ac02c
Manuel Tondeur <manueltondeur@gmail.com>
2012-04-24 18:07:13 +0200

    Ajout des fichiers de paramétrisation par Maëlle.

---
+++ include/projetZTrackerParametrisation.hh
@@ -0,0 +1,56 @@
+
+#ifndef projetZTrackerParametrisation_H
+#define projetZTrackerParametrisation_H 1
+
+#include "globals.hh"
+#include "G4VPVParameterisation.hh"
+
+class G4VPhysicalVolume;
+class G4Tubs;
+
+// Dummy declarations to get rid of warnings ...
+class G4Trd;
+class G4Trap;
+class G4Cons;
+class G4Orb;
+class G4Sphere;
+class G4Torus;
+class G4Para;
+class G4Hype;
+class G4Box;
+class G4Polycone;
+class G4Polyhedra;
+
+class projetZTrackerParametrisation : public G4VPVParameterisation
+{ 
+  public:
+  
+    projetZTrackerParametrisation();
+    virtual ~projetZTrackerParametrisation();
+    void ComputeTransformation(const G4int copyNo,
+                                     G4VPhysicalVolume *physVol) const;
+    void ComputeDimensions(      G4Tubs & trackerLayer,
+                           const G4int copyNo,
+                           const G4VPhysicalVolume * physVol) const;
+
+  private:  // Dummy declarations to get rid of warnings ...
+
+    void ComputeDimensions (G4Trd&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Trap&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Cons&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Orb&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Sphere&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Torus&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Para&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Hype&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Box&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Polycone&,const G4int,const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Polyhedra&,const G4int,const G4VPhysicalVolume*) const {}
+
+  private:
+
+#include "projetZTrackerParameterDef.hh"
+
+};
+
+#endif
---
+++ src/projetZTrackerParametrisation.cc
@@ -0,0 +1,34 @@
+
+
+#include "projetZTrackerParametrisation.hh"
+
+#include "G4VPhysicalVolume.hh"
+#include "G4ThreeVector.hh"
+#include "G4Tubs.hh"
+
+projetZTrackerParametrisation::projetZTrackerParametrisation()
+{
+
+#include "projetZTrackerParameterDef.hh"
+
+}
+
+projetZTrackerParametrisation::~projetZTrackerParametrisation()
+{;}
+
+void projetZTrackerParametrisation::ComputeTransformation
+(const G4int, G4VPhysicalVolume* physVol) const
+{
+  G4ThreeVector origin;
+  physVol->SetTranslation(origin);
+}
+
+void projetZTrackerParametrisation::ComputeDimensions
+(G4Tubs& trackerLayer, const G4int copyNo, const G4VPhysicalVolume*) const
+{
+  trackerLayer.SetInnerRadius(tracker_radius[copyNo]);
+  trackerLayer.SetOuterRadius(tracker_radius[copyNo]+tracker_thick);
+  trackerLayer.SetZHalfLength(tracker_length[copyNo]);
+  trackerLayer.SetStartPhiAngle(trkTubs_sphi,false);
+  trackerLayer.SetDeltaPhiAngle(trkTubs_dphi);
+}
